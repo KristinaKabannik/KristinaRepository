da6d67f27b5d220bd98eaa57a7bb29fcff49c3d2
Kristina <twinkles.kiev@gmail.com>
2024-10-26 13:05:04 +0300

    Added files to lesson_09 - version 2

--- homeworks_version2.py
+++ homeworks_version2.py
@@ -5,12 +5,13 @@
 # які присутні в lst1. Данні в лісті можуть бути будь якими.
 ''' Виконання за допомогою функції:'''
 
-def new_list(lst1):
+
+def test_new_list(lst1):
     lst2 = [element for element in lst1 if isinstance(element, str)] #проходимо по списку і визначаємо, чи є елемент стрінговим
     return lst2
 
 lst1 = ['1', '2', 3, True, 'False', 5, '6', 7, 8, 'Python', 9, 0, 'Lorem Ipsum', 1.345, 'Hello']
-lst2 = new_list(lst1)
+lst2 = test_new_list(lst1)
 print(lst2)
 
 
@@ -22,12 +23,12 @@ print(lst2)
 # Є ліст з числами, порахуйте сумму усіх ПАРНИХ чисел в цьому лісті
 ''' Виконання за допомогою функції:'''
 
-def sum_ev_numb(lst):
+def test_sum_ev_numb(lst):
     even_numbers_sum = sum(number for number in lst if number % 2 == 0)  # проходимо по списку і сумуємо парні числа
     return even_numbers_sum
 
 lst = [1, 2, 3, 6, 9, 5, 6, 7, 8]
-even_numbers_sum = sum_ev_numb(lst)
+even_numbers_sum = test_sum_ev_numb(lst)
 print(f"Сума усіх парних чисел: {even_numbers_sum}")
 
 
@@ -41,8 +42,8 @@ print(f"Сума усіх парних чисел: {even_numbers_sum}")
 # Строку отримати за допомогою функції input()
 ''' Виконання за допомогою функції:'''
 
-def check_unique_symbols():
-    symbols = input("Input some symbols: ")  # вводимо строку
+def test_check_unique_symbols():
+    symbols = "qwe rty uio pasd"  # строкa
     unique_symbols = set(symbols)       # Створюємо множину для зберігання унікальних символів
 
     if len(unique_symbols) > 10:       # Порівнюємо кількість унікальних символів
@@ -50,8 +51,7 @@ def check_unique_symbols():
     else:
         print(False)
 
-check_unique_symbols()
-
+test_check_unique_symbols()
 
 
 
@@ -64,9 +64,9 @@ check_unique_symbols()
 # якщо користувач ввів слово без букви "h".
 ''' Виконання за допомогою функції:'''
 
-def need_h():
+def test_need_h():
     while True:
-        word = input("Введіть слово, в якому є буква 'h'(or 'H'): ")    # вводимо слово
+        word = "hello"    # вводимо слово
         if 'h' in word.lower():   # Перевіряємо, чи є в слові 'h' або 'H'
             print("Правильно! Буква 'h' присутя")
             break  # Завершуємо цикл, якщо умова виконана
@@ -74,4 +74,4 @@ def need_h():
             print("Відсутня буква 'h' ('H'). Введіть слово ще раз:")
 
 
-need_h()
+test_need_h()
