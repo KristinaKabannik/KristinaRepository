a062bbb2f31a5c617ca7d4c649d72af2e1f5da56
Kristina <twinkles.kiev@gmail.com>
2024-11-23 18:31:58 +0200

    The '__init__.py' file is fixed and the 'test'py' file is added

---
+++ lesson17/randominfo/randominfo-master/test.py
@@ -0,0 +1,9 @@
+import randominfo
+person = randominfo.Person()
+print(person.full_name, person.gender, person.country, person.address)
+
+
+# переписана функція def get_address()
+# доданий метод def birthdate(self) в клас Person
+# доданий метод def get_email(self) в клас Person
+
